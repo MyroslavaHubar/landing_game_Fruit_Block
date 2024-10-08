6345434053df50a4a1f196ed768f35bd33d86f39
Miami <alex.miami.petrov@gmail.com>
2024-10-08 15:32:31 +0300

    m

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -2,16 +2,48 @@ import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
 
+// const swiper = new Swiper('.gallerySwiper', {
+//   modules: [Navigation, Pagination],
+//   effect: 'coverflow',
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: 'auto',
+//   coverflowEffect: {
+//     rotate: 30,
+//     stretch: 0,
+//     depth: 100,
+//     modifier: 1,
+//     slideShadows: true,
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+//   rewind: true,
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 1,
+//       spaceBetween: 0,
+//     },
+//     1200: {
+//       slidesPerView: 3,
+//       spaceBetween: 0,
+//     },
+//   },
+// });
+
 const swiper = new Swiper('.gallerySwiper', {
   modules: [Navigation, Pagination],
-  slidesPerView: 1,
-  spaceBetween: 0,
   effect: 'coverflow',
   grabCursor: true,
   centeredSlides: true,
-  slidesPerView: 'auto',
+  slidesPerView: 3, // Встановлюємо три слайди
   coverflowEffect: {
-    rotate: 0,
+    rotate: 30,
     stretch: 0,
     depth: 100,
     modifier: 1,
@@ -37,19 +69,11 @@ const swiper = new Swiper('.gallerySwiper', {
     },
   },
 });
-// const swiper = new Swiper('.gallerySwiper', {
-//   effect: 'coverflow',
-//   grabCursor: true,
-//   centeredSlides: true,
-//   slidesPerView: 'auto',
-//   coverflowEffect: {
-//     rotate: 0,
-//     stretch: 0,
-//     depth: 100,
-//     modifier: 1,
-//     slideShadows: true,
-//   },
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-// });
+
+// Додайте клас center-slide до активного слайду
+swiper.on('slideChange', () => {
+  const slides = document.querySelectorAll('.swiper-slide');
+  slides.forEach(slide => slide.classList.remove('center-slide')); // Видаляємо клас у всіх слайдів
+  const activeIndex = swiper.activeIndex;
+  slides[activeIndex].classList.add('center-slide'); // Додаємо клас до активного слайду
+});
