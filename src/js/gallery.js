410aa504bbf208967a6adbb917e93d39dc0e38fa
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-10-09 03:32:19 +0200

    add gallery

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -201,3 +201,35 @@ if (initialSlides[swiper.initialSlide]) {
 
 // exampleCarousel.setControls();
 // exampleCarousel.useControls();
+
+// =================NEW======================//
+// import Swiper from 'swiper/bundle';
+// import 'swiper/scss';
+
+// const swiper = new Swiper('.gallerySwiper', {
+//   loop: true,
+//   effect: 'coverflow',
+//   speed: 800,
+//   breakpoints: {
+//     320: {
+//       centeredSlides: false,
+//       slidesPerView: 1,
+//     },
+//     1200: {
+//       grabCursor: true,
+//       centeredSlides: true,
+//       slidesPerView: 'auto',
+//       coverflowEffect: {
+//         rotate: 0,
+//         stretch: -0.8,
+//         depth: 20,
+//         modifier: 10,
+//         slideShadows: true,
+//       },
+//     },
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
