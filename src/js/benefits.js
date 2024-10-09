fbe032fb87f2e2baec199db630344823f70df91f
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-10-09 03:29:02 +0200

    create benefits

---
+++ src/js/benefits.js
@@ -0,0 +1,6 @@
+let benefitCard = document.querySelector('.benefits-card');
+let benefitModal = document.querySelector('.benefit-modal');
+
+benefitCard.onClick = function () {
+  benefitModal.style.display = 'block';
+};
