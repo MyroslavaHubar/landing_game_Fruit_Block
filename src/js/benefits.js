45436a9f373279e04dbb24e3c8dd654b47cb72ef
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-10-09 10:10:05 +0200

    add benefit modal

--- src/js/benefits.js
+++ src/js/benefits.js
@@ -1,6 +1,13 @@
-let benefitCard = document.querySelector('.benefits-card');
-let benefitModal = document.querySelector('.benefit-modal');
+const benefitCard = document.querySelector('#card-4');
+const benefitModal = document.querySelector('#benefit-modal');
 
-benefitCard.onClick = function () {
-  benefitModal.style.display = 'block';
-};
+benefitCard.addEventListener('click', () => {
+  benefitModal.classList.add('benefit-modal-show');
+});
+
+benefitModal.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target === benefitModal) {
+    benefitModal.classList.remove('benefit-modal-show');
+  }
+});
