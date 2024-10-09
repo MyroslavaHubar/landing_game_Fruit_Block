3b421beeb139d37d94a2016eca39f46667fecce7
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-10-09 10:36:39 +0200

    add new modal for section faq

--- src/js/benefits.js
+++ src/js/benefits.js
@@ -1,13 +1,39 @@
-const benefitCard = document.querySelector('#card-4');
-const benefitModal = document.querySelector('#benefit-modal');
+const benefitCard_4 = document.querySelector('#card-4');
+const benefitCard_5 = document.querySelector('#card-5');
+const benefitCard_6 = document.querySelector('#card-6');
+const benefitModal_4 = document.querySelector('#benefit-modal-4');
+const benefitModal_5 = document.querySelector('#benefit-modal-5');
+const benefitModal_6 = document.querySelector('#benefit-modal-6');
 
-benefitCard.addEventListener('click', () => {
-  benefitModal.classList.add('benefit-modal-show');
+benefitCard_4.addEventListener('click', () => {
+  benefitModal_4.classList.add('benefit-modal-show');
 });
 
-benefitModal.addEventListener('click', e => {
+benefitCard_5.addEventListener('click', () => {
+  benefitModal_5.classList.add('benefit-modal-show');
+});
+
+benefitCard_6.addEventListener('click', () => {
+  benefitModal_6.classList.add('benefit-modal-show');
+});
+
+benefitModal_4.addEventListener('click', e => {
   e.preventDefault();
-  if (e.target === benefitModal) {
-    benefitModal.classList.remove('benefit-modal-show');
+  if (e.target === benefitModal_4) {
+    benefitModal_4.classList.remove('benefit-modal-show');
+  }
+});
+
+benefitModal_5.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target === benefitModal_5) {
+    benefitModal_5.classList.remove('benefit-modal-show');
+  }
+});
+
+benefitModal_6.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target === benefitModal_6) {
+    benefitModal_6.classList.remove('benefit-modal-show');
   }
 });
